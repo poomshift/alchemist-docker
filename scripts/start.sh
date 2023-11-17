e2588d7256e497c1e00558411bbedef11a2f2814
promptalchemist <130088861+poomshift@users.noreply.github.com>
2023-11-17 17:24:34 +0700

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,12 +5,6 @@ set -e  # Exit the script if any statement returns a non-true return value
 #                          Function Definitions                                #
 # ---------------------------------------------------------------------------- #
 
-# Start nginx service
-start_nginx() {
-    echo "Starting Nginx service..."
-    service nginx start
-}
-
 # Execute script if exists
 execute_script() {
     local script_path=$1
@@ -32,31 +26,6 @@ setup_ssh() {
     fi
 }
 
-# Export env vars
-export_env_vars() {
-    echo "Exporting environment variables..."
-    printenv | grep -E '^RUNPOD_|^PATH=|^_=' | awk -F = '{ print "export " $1 "=\"" $2 "\"" }' >> /etc/rp_environment
-    echo 'source /etc/rp_environment' >> ~/.bashrc
-}
-
-# Start jupyter lab
-start_jupyter() {
-    if [[ $JUPYTER_PASSWORD ]]; then
-        echo "Starting Jupyter Lab..."
-        mkdir -p /workspace && \
-        cd / && \
-        nohup jupyter lab --allow-root \
-          --no-browser \
-          --port=8888 \
-          --ip=* \
-          --FileContentsManager.delete_to_trash=False \
-          --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' \
-          --ServerApp.token=${JUPYTER_PASSWORD} \
-          --ServerApp.allow_origin=* \
-          --ServerApp.preferred_dir=/workspace &> /workspace/logs/jupyter.log &
-        echo "Jupyter Lab started"
-    fi
-}
 
 # ---------------------------------------------------------------------------- #
 #                               Main Program                                   #
@@ -64,16 +33,11 @@ start_jupyter() {
 
 start_nginx
 
-execute_script "/pre_start.sh" "Running pre-start script..."
+execute_script "/start_a1111.sh" "Running start_a1111 script..."
 
-echo "Pod Started"
+echo "Started"
 
 setup_ssh
-start_jupyter
-export_env_vars
 
-execute_script "/post_start.sh" "Running post-start script..."
-
-echo "Container is READY!"
 
 sleep infinity
