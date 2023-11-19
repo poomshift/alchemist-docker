8649ed558a706748889684e44e9ec784b2e4c253
promptalchemist <130088861+poomshift@users.noreply.github.com>
2023-11-19 19:22:02 +0700

    Update webui-user.sh

--- a1111/webui-user.sh
+++ a1111/webui-user.sh
@@ -10,7 +10,7 @@ install_dir="/workspace"
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--port 3001 --skip-install --listen --api --xformers --enable-insecure-extension-access --no-half-vae --theme dark"
+export COMMANDLINE_ARGS="--skip-install --listen --api --xformers --enable-insecure-extension-access --no-half-vae --theme dark"
 
 # python3 executable
 #python_cmd="python3"
